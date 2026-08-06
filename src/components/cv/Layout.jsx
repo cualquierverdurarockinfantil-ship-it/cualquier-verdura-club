73b933048affac2991540f2c06f8fbadb5f6c2df
cualquierverdurarockinfantil-ship-it <cualquierverdurarockinfantil@gmail.com>
2026-08-06 17:12:38 -0300

    Agrega sistema de banners y campanas

--- src/components/cv/Layout.jsx
+++ src/components/cv/Layout.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import PlayerBar, { PlayerSpacer } from "./PlayerBar";
 import PlayChoiceModal from "./PlayChoiceModal";
+import CampaignBanner from "./CampaignBanner";
 import { Outlet } from "react-router-dom";
 import { motion } from "framer-motion";
 import { MusicPlayerProvider } from "@/context/MusicPlayerContext";
@@ -10,6 +11,7 @@ export default function Layout() {
   return (
     <MusicPlayerProvider>
       <div className="min-h-screen bg-white flex flex-col">
+        <CampaignBanner />
         <Navbar />
         <main className="pt-16 flex-1">
           <motion.div
